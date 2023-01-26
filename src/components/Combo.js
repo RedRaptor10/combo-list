a62e7b087121fc343a31d65b86fbd63deb0e5c54
RedRaptor10 <weiminh_lam@yahoo.com>
2023-01-26 15:09:33 -0600

    Add fetchCombo function

--- src/components/Combo.js
+++ src/components/Combo.js
@@ -1,6 +1,33 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 const Combo = () => {
+    const { comboId } = useParams();
+    const [combo, setCombo] = useState();
+
+    // Get Combo
+    useEffect(() => {
+        const fetchCombo = async () => {
+            const options = {
+                method: 'GET'
+            };
+
+            const data = await fetch(process.env.REACT_APP_SERVER + 'api/combos/' + comboId, options);
+            return data.json();
+        };
+
+        fetchCombo()
+        .then(res => {
+            setCombo(res);
+        });
+    }, [comboId]);
+
     return (
-        <div>Combo</div>
+        <div>
+            {combo ?
+                <div>{combo.input}</div>
+            : null}
+        </div>
     );
 };
 
