0c03de04f0923f5874cb4a664fc761db57ab55c1
RedRaptor10 <weiminh_lam@yahoo.com>
2023-02-02 05:04:26 -0600

    Create Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
 import Home from './Home';
 import Combo from './Combo';
 
 const App = () => {
     return (
         <HashRouter>
+            <Header />
             <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/combos/:comboId" element={<Combo />} />
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header>
+            <h1><Link to="/">Combo List</Link></h1>
+        </header>
+    );
+};
+
+export default Header;
