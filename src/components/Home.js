0a478f3dfc83b25608b62ef79a84e4398377facb
RedRaptor10 <weiminh_lam@yahoo.com>
2023-02-03 04:38:03 -0600

    Add fetchCharacters to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,47 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
+    const [characters, setCharacters] = useState();
+
+    // Get Characters
+    useEffect(() => {
+        const fetchCharacters = async () => {
+            try {
+                const options = {
+                    method: 'GET'
+                };
+
+                const data = await fetch(process.env.REACT_APP_SERVER + 'api/characters', options);
+                if (data.status < 200 || data.status > 299) {
+                    throw new Error('Error ' + data.status + ': ' + data.statusText);
+                }
+                return data.json();
+            } catch (error) {
+                throw new Error(error);
+            }
+        };
+
+        fetchCharacters()
+        .then(res => {
+            setCharacters(res);
+        });
+    }, []);
     return (
-        <div>Home</div>
+        <main className="home">
+            <h2>Characters</h2>
+            <section className="characters">
+                {characters ?
+                    characters.map(character => {
+                        return (
+                            <Link key={character._id} to={'/' + character.slug} className="character-wrapper">
+                                <div className="character">{character.name}</div>
+                            </Link>
+                        )
+                    })
+                : null}
+            </section>
+        </main>
     );
 };
 
