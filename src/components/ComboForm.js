c4cef660e3dfff1d76375e3cb9e310598e5ffcb6
RedRaptor10 <weiminh_lam@yahoo.com>
2023-02-08 13:09:19 -0600

    Add form object, handleChange, and addCombo function

--- src/components/ComboForm.js
+++ src/components/ComboForm.js
@@ -1,20 +1,73 @@
+import { useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+
 const ComboForm = () => {
+    const { character } = useParams();
+    const [form, setForm] = useState({
+        damage: '',
+        input: '',
+        notes: '',
+        type: 'Midscreen'
+    });
+    const navigate = useNavigate();
+
+    const handleChange = event => {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const addCombo = event => {
+        event.preventDefault();
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                character: character,
+                damage: form.damage,
+                date: new Date(),
+                input: form.input,
+                notes: form.notes,
+                tags: [],
+                type: form.type
+            })
+        }
+
+        fetch(process.env.REACT_APP_SERVER + 'api/combos/create', options)
+        .then(res => {
+            if (res.status < 200 || res.status > 299) {
+                throw new Error('Error ' + res.status + ': ' + res.statusText);
+            }
+            return res.json();
+        })
+        .then(() => {
+            navigate('/' + character);
+        })
+        .catch(error => {
+            throw new Error(error);
+        });
+    }
+
     return (
         <main className="combo-form-page">
             <form className="combo-form" action="">
                 <label htmlFor="input">Input</label>
-                <input name="input"></input>
+                <input name="input" value={form.input} onChange={handleChange}></input>
                 <label htmlFor="damage">Damage</label>
-                <input name="damage"></input>
+                <input name="damage" value={form.damage} onChange={handleChange}></input>
                 <label htmlFor="notes">Notes</label>
-                <textarea name="notes"></textarea>
+                <textarea name="notes" value={form.notes} onChange={handleChange}></textarea>
                 <label htmlFor="tags">Tags</label>
                 <label htmlFor="type">Type</label>
-                <select name="type">
+                <select name="type" value={form.type} onChange={handleChange}>
                     <option value="Midscreen">Midscreen</option>
                     <option value="Corner">Corner</option>
                 </select>
-                <button className="submit-btn" type="submit">Add</button>
+                <button className="submit-btn" type="submit" onClick={addCombo}>Add</button>
             </form>
         </main>
     );
